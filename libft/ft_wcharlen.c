a6c497f09827f9244b819382553c813911981035
Dagoberto Montoya <dmontoya@e1z3r10p4.42.us.org>
2017-11-30 23:39:56 -0800

    jf

--- libft/ft_wcharlen.c
+++ libft/ft_wcharlen.c
@@ -15,8 +15,19 @@
 int		ft_wcharlen(wchar_t c)
 {
 	int len;
+	unsigned int ud;
+	
+	ud = (unsigned int)c;
 
-	if (c < 0x7F)
+	if (ud <= 127)
+		len = 1;
+	else if (ud <= 2047)
+		len = 2;
+	else if (ud <= 65535)
+		len = 3;
+	else
+		len = 4;
+/*	if (c < 0x7F)
 		len = 1;
 	else if (c < 0x7FF)
 		len = 2;
@@ -25,6 +36,6 @@ int		ft_wcharlen(wchar_t c)
 	else if (c < 0x10FFFF)
 		len = 4;
 	else
-		len = 0;
+		len = 0;*/
 	return (len);
 }
